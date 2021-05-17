b27eb37e26afb005505d0e8e1325d43694b09594
amisha <rajputamisha34@gmail.com>
2021-05-17 15:13:55 +0530

    completed

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,10 +1,14 @@
 import React, { Fragment, useState } from 'react'
 import axios from 'axios';
-
+import Message from './Message'
+import Progress from './Progress'
 export const FileUpload = () => {
   const[file, setFile] = useState('');
   const[fileName, setFilename] = useState('Choose File');
   const [uploadedFile, setUploadedFile] = useState({});
+  const[message, setMessage] = useState('');
+   const [uploadPercentage, setUploadPercentage] = useState(0);
+
   const onChange = e => {
       setFile(e.target.files[0]);
       setFilename(e.target.files[0].name);
@@ -19,29 +23,43 @@ export const FileUpload = () => {
           const res = await axios.post('/upload', formData, {
               headers:{
                   'Content-Type' : 'multipart/form-data'
+              },
+              onUploadProgress: progressEvent =>{
+                setUploadPercentage(parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total)))
+
+                //clear percentage
+            setTimeout(() => setUploadPercentage(0), 10000);
+
               }
+            
+
           });
         
           const { fileName, filePath} = res.data;
           setUploadedFile({ fileName, filePath});
+
+          setMessage('File Uploaded');
       } catch(err){
           if(err.response.status === 500){
-              console.log('There was a problem with the server');
+              setMessage('There was a problem with the server');
             }
             else{
-                console.log(err.response);
+                setMessage(err.response);
             }
       }
   };
 
     return (
         <Fragment>
+            {message ? <Message msg={message} /> : null}
             <form onSubmit={onSubmit}>
             <div className="input-group">
   <input type="file" className="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload" onChange={onChange} />
   
 </div>
+<br />
 
+<Progress percentage={uploadPercentage} />
 <br />
        
 <div className="d-grid gap-2">
